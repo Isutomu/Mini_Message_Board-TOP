95711792479ac4f1dc7ebe6ff9acd353d9251952
Isutomu <isutomu@gmail.com>
2024-09-24 10:59:00 -0300

    :sparkles: feat: Add root route and render view

--- app.mjs
+++ app.mjs
@@ -1,10 +1,27 @@
 import express from "express";
+import "dotenv/config";
+import path from "node:path";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+app.set("views", path.join(import.meta.dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("yahalo!");
+  res.render("messages", { messages });
 });
 
 app.listen(PORT, () =>
