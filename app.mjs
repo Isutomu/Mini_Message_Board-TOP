46acf64f3775a12ea4baa5ca730b34dbf6bcdf5a
Isutomu <isutomu@gmail.com>
2024-09-24 11:47:39 -0300

    :sparkles: feat: Add message details view

--- app.mjs
+++ app.mjs
@@ -1,16 +1,19 @@
 import express from "express";
 import "dotenv/config";
 import path from "node:path";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const messages = [
   {
+    id: uuidv4(),
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: uuidv4(),
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -29,11 +32,18 @@ app.get("/new", (req, res) => {
 });
 app.post("/new", (req, res) => {
   messages.push({
+    id: uuidv4(),
     text: req.body.messageText,
     user: req.body.messageUser,
     added: new Date(),
   });
   res.redirect("/");
+});
+app.get("/message/:id", (req, res) => {
+  const openedMessage = messages.filter(
+    (message) => message.id === req.params.id
+  )[0];
+  res.render("messageDetails", { message: openedMessage });
 });
 
 app.listen(PORT, () =>
