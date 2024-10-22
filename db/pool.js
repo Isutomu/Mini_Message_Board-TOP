cc6043f057c92596d759b887f416639c69ece95c
Isutomu <isutomu@gmail.com>
2024-10-22 20:12:26 -0300

    :bug: fix: Change user definition on Pool.

--- db/pool.js
+++ db/pool.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 module.exports = new Pool({
   host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USER,
+  user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   ssl: true,
 });
