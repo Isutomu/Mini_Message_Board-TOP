63067984d79fa0a548ab68f9e4276676df21cc98
Isutomu <isutomu@gmail.com>
2024-10-22 19:34:39 -0300

    :bug: fix: Add SSL true to Pool.

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,10 @@
 const { Pool } = require("pg");
+const { ssl } = require("pg/lib/defaults");
 
 module.exports = new Pool({
   host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
+  ssl: true,
 });
